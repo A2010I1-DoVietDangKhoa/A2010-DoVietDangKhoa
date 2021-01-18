4fcd5c4f685b81184c467f67de2010d1c9106158
zzkigzz48 <dokhoa342@gmail.com>
2021-01-18 19:58:02 +0700

    bai tap

--- Module2/src/bai4_Lop_va_doi_tuong_trong_Java/bai_tap/XayDungLopStopWatch.java
+++ Module2/src/bai4_Lop_va_doi_tuong_trong_Java/bai_tap/XayDungLopStopWatch.java
@@ -1,26 +1,25 @@
 package bai4_Lop_va_doi_tuong_trong_Java.bai_tap;
 
-import java.util.Date;
 import java.util.Scanner;
 
 public class XayDungLopStopWatch {
-    public static class StopWatch{
+    public static class StopWatch {
         private long startTime;
         private long endTime;
 
-        public StopWatch(){
+        public StopWatch() {
 
         }
 
-        public void start(){
+        public void start() {
             startTime = System.currentTimeMillis();
         }
 
-        public void end(){
+        public void end() {
             endTime = System.currentTimeMillis();
         }
 
-        public long getElapsedTime(){
+        public long getElapsedTime() {
             return endTime - startTime;
         }
     }
@@ -30,25 +29,24 @@ public class XayDungLopStopWatch {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Start stop watch? Y/N");
         char choice = scanner.next().charAt(0);
-        switch (choice){
+        switch (choice) {
             case 'Y':
             case 'y':
                 stopWatch.start();
                 System.out.println("Counting...");
                 System.out.println("Input \"show\" if you want to show elapsed time, \"stop\" if you want to stop.");
                 String command;
-                do{
+                do {
                     command = scanner.nextLine();
-                    if (command.equals("show")){
+                    if (command.equals("show")) {
                         stopWatch.end();
-                        System.out.println(stopWatch.getElapsedTime()/1000.0);
-                    }
-                    else if (command.equals("stop")){
+                        System.out.println(stopWatch.getElapsedTime() / 1000.0);
+                    } else if (command.equals("stop")) {
                         break;
                     }
-                }while (true);
+                } while (true);
                 stopWatch.end();
-                System.out.println(stopWatch.getElapsedTime()/1000.0 + " seconds");
+                System.out.println(stopWatch.getElapsedTime() / 1000.0 + " seconds");
                 break;
             case 'N':
             case 'n':
