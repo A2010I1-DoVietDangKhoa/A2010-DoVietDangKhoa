0aba03ccc668eb7e3ad9a71666cfeed96c0f602c
zzkigzz48 <dokhoa342@gmail.com>
2021-01-27 19:10:23 +0700

    bai tap

--- Module2/src/bai6_Ke_thua/bai_tap/ThietKeVaTrienKhaiLopTriangle/TestShape.java
+++ Module2/src/bai6_Ke_thua/bai_tap/ThietKeVaTrienKhaiLopTriangle/TestShape.java
@@ -3,10 +3,14 @@ package bai6_Ke_thua.bai_tap.ThietKeVaTrienKhaiLopTriangle;
 public class TestShape {
     public static void main(String[] args) {
         Shape shape = new Shape("red", true);
+        System.out.println(shape.getColor());
+        System.out.println(shape.isFilled());
         System.out.println(shape.toString());
 
         shape.setFilled(false);
         shape.setColor("yellow");
+        System.out.println(shape.getColor());
+        System.out.println(shape.isFilled());
         System.out.println(shape.toString());
     }
 }
