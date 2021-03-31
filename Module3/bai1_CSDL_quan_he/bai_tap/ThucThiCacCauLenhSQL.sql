e21732d07bc630173df33bd79f3ec8c44badf2c8
zzkigzz48 <dokhoa342@gmail.com>
2021-03-31 20:29:25 +0700

    bai tap

---
+++ Module3/bai1_CSDL_quan_he/bai_tap/ThucThiCacCauLenhSQL.sql
@@ -0,0 +1,29 @@
+use classicmodels;
+
+SELECT * FROM customers;
+
+SELECT customerName, phone, city, country FROM customers;
+
+SELECT * FROM customers WHERE customerName = 'Atelier Graphique';
+
+SELECT * FROM customers WHERE customername like '%A%';
+
+SELECT * FROM customers WHERE city IN ('Nantes',' Las Vegas',' Warszawa','NYC');
+
+insert into customers 
+values (109,'Tran Dan','Tran','Dan','0123.456.789','193 Nguyen Luong Bang',null,'Da Nang',null,'55000','Vietnam',1370,150000.0);
+
+insert into customers (customerNumber, customerName, contactFirstName ,contactLastName, phone, addressLine1, city, country)
+values (111, 'Lmao', 'Buh', 'Dak', '00000000000', 'nope', 'nope', 'nope' );
+
+select customerName
+from customers
+where customerNumber = 111;
+
+update customers
+set customerName = 'Dak Buh Lmao'
+where customerName = 'Atelier Graphique';
+
+
+alter table customers
+add column customerEmail varchar(30);
